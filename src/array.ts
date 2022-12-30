bc9c31cf3bcb00b64e031931272352ee245c0a89
zenklot <zenklot11@gmail.com>
2022-12-30 16:07:00 +0700

    array & tuples

---
+++ src/array.ts
@@ -0,0 +1,14 @@
+// Array
+let array : number[] = [1,2,3]
+array = [4, 5, 6]
+
+let array2 : string[] = ["A", "B", "C", "D"];
+array2 = ["1", "2", "3"];
+
+let array3 : any[];
+array3 = [1, 2, "A", false, {}];
+
+// tuples
+let biodata : [string, number];
+biodata = ["Supri", 12 ];
+biodata = ["Ciamis", 1998];
