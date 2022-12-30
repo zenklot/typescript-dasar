2bec75b6782e2d144d9f0d48cd98daa976f01a85
zenklot <zenklot11@gmail.com>
2022-12-30 16:02:14 +0700

    Type Data Dasar

---
+++ build/typeDasar.js
@@ -0,0 +1,20 @@
+"use strict";
+// STRING
+// Secara otomatis di inisialkan dengan Type String
+let nama = "Supriatna";
+// Ketika di ubah ke type data lain error
+// nama = 12;
+let myName = "Supri";
+myName = "Supriatna";
+// NUMBER
+let umur = 12;
+umur = 30;
+// BOOLEAN
+let pria = false;
+pria = true;
+// ANY
+let heroes = "Iron Man";
+heroes = 19;
+heroes = false;
+heroes = {};
+heroes = [];
